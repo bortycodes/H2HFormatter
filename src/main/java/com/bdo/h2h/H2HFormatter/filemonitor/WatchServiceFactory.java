fc5d318b2c970d871f2834cd007fd60e3b302dde
borty <gpborla+github@gmail.com>
2023-01-29 16:14:49 +0800

    Add WatchService Factory

---
+++ src/main/java/com/bdo/h2h/H2HFormatter/filemonitor/WatchServiceFactory.java
@@ -0,0 +1,23 @@
+package com.bdo.h2h.H2HFormatter.filemonitor;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.WatchService;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class WatchServiceFactory {
+	private static WatchService watchService;
+	
+	public static WatchService getInstance() {
+		if (watchService == null) {
+			try {
+				watchService = FileSystems.getDefault().newWatchService();
+			} catch (IOException e) {
+				throw new RuntimeException("Error initializing watch service", e);
+			}
+		}
+		return watchService;
+	}
+}
