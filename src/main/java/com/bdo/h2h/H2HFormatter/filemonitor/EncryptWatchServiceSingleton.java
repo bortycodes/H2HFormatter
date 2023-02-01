2000c830621d142da8199c99acdae5e44669de69
borty <gpborla@gmail.com>
2023-02-01 17:21:19 +0800

    Add WatchService for processed files to be encrypted.

---
+++ src/main/java/com/bdo/h2h/H2HFormatter/filemonitor/EncryptWatchServiceSingleton.java
@@ -0,0 +1,26 @@
+package com.bdo.h2h.H2HFormatter.filemonitor;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.WatchService;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EncryptWatchServiceSingleton {
+	private static WatchService watchService;
+	
+	private EncryptWatchServiceSingleton() {}
+	
+	public WatchService getInstance() {
+		if (watchService == null) {
+			try {
+				watchService = FileSystems.getDefault().newWatchService();
+			} catch (IOException e) {
+				e.printStackTrace();
+				throw new RuntimeException("Error initializing watch service", e);
+			}
+		}
+		return watchService;
+	}
+}
