3e69aae2af510d2ac01b8f97179527430e4b9808
borty <gpborla+github@gmail.com>
2023-02-02 02:34:47 +0800

    Add watch service for decrypted files.

---
+++ src/main/java/com/bdo/h2h/H2HFormatter/filemonitor/DecryptedFilesWatchService.java
@@ -0,0 +1,24 @@
+package com.bdo.h2h.H2HFormatter.filemonitor;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.WatchService;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DecryptedFilesWatchService {
+	private static WatchService watchService;
+	
+	public static WatchService getInstance() {
+		if (watchService == null) {
+			try {
+				watchService = FileSystems.getDefault().newWatchService();
+			} catch (IOException e) {
+				e.printStackTrace();
+				throw new RuntimeException("Error initializing watch service", e);
+			}
+		}
+		return watchService;
+	}
+}
